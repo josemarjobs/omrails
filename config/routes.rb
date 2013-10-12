e805938365c559fa61da35ab749ea20cec9bf2b5
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2013-10-12 01:32:48 -0300

    Add home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Onrails::Application.routes.draw do
+  get "about" => "pages#about"
+  
+  get "pages/about"
+
+  root to: "pages#home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
